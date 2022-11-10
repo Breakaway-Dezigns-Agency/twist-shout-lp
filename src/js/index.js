a8b325fdb242807904c69700ed3b69d347f2f5c9
Brian Garcia <bgarcia@madebyxds.com>
2022-11-10 00:17:11 -0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+
+let acc = document.getElementsByClassName('accordion')
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function() {
+    this.classList.toggle('active')
+    let panel = this.nextElementSibling
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none'
+    } else {
+      panel.style.display = 'block'
+    }
+  })
+}
+
+var year = document.querySelector('#year');
+
+year.innerHTML = new Date().getFullYear()
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import '../styles/main.scss'
+import './vendor/slick.js'
+import './index.js'
